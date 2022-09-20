f6e31e523ac4b9b0e46bfa52bb15f342037294af
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-09-19 23:06:13 -0400

    fixed image issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <main>
-        <img className="bg" src={schoolOfAthens} />
+        <img src={Logo} class="logo" />
       </main>
     </div>
   );
