563e66ea6a1f36ea2efedbec1d79525bb7ac4403
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-10-14 22:43:39 -0400

    Changed the button color

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,5 +1,6 @@
 import "./Button.css";
 const Button = (props) => {
-  return <button className={props.color}>{props.name}</button>;
+  let bg = props.color == "yellow" ? "bg-yellow-300" : "bg-blue-500";
+  return <button className={bg}>{props.name}</button>;
 };
 export default Button;
