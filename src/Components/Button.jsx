3f3b4ce55a25caac0f2fd89f15e961b4db3e7e3f
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-10-15 22:44:06 -0400

    removed all

--- src/Components/Button.jsx
+++
@@ -1,6 +0,0 @@
-import "./Button.css";
-const Button = (props) => {
-  let bg = props.color == "yellow" ? "bg-yellow-300" : "bg-blue-500";
-  return <button className={bg}>{props.name}</button>;
-};
-export default Button;
