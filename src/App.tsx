946ab14f5a165fc46e388ee0ed991ebc58f0d941
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-10-22 19:10:19 -0400

    tailwind is working again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="bg-black w-screen h-screen text-slate-50 text-center">
-      Hello world
+      <h1 className="bg-white">Hello World</h1>
     </div>
   )
 };
