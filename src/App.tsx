73fce2ea502a39155e728973d1e8ec9d99a0fb3a
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-10-27 13:21:19 -0400

    changed name of the logo, and imported logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Name from "./Components/Name"
+import Logo from "./assets/Logo.png"
 const App = () => {
   return (
     <div className="bg-white dark:bg-black w-screen h-screen">
-      <Name h1="font-robot text-black bg-blue-200" div="bg-red-200"/>
+      <Name h1="font-robot text-black" div="bg-red-200"/>
+      <img className="w-100 h-100" src={Logo} />
     </div>
   )
 };
