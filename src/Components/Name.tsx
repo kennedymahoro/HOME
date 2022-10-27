0dc1cd209ea5fe76df8b1ba78318f15a3fb1097f
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-10-26 20:35:14 -0400

    fixed ununique keys and changed text-color

--- src/Components/Name.tsx
+++ src/Components/Name.tsx
@@ -1,15 +1,15 @@
 const Name = () => {
+    let int = 0;
     const FirstNameStr = "KENNEDY";
     const SecondNameStr = "MAHORO";
-    const FirstNameArray = FirstNameStr.split("");
-    const SecondNameArray = SecondNameStr.split("");
-    console.log(FirstNameArray)
+    const FirstNameArray = FirstNameStr.split("").map(a => (<span key={int++}>{a}</span>))
+    const SecondNameArray = SecondNameStr.split("").map(a => (<span key={int++}>{a}</span>))
 
 
     return (
         <div>
-            <h1 className="font-haruno">{FirstNameArray}</h1>
-            <h1 className="font-haruno">{SecondNameArray}</h1>
+            <h1 className="font-haruno text-black">{FirstNameArray}</h1>
+            <h1 className="font-haruno text-black">{SecondNameArray}</h1>
         </div>
     )
 }
