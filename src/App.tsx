606dcf17a7c62a3cccf4b692a80b233fc5caffe5
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-10-27 21:30:20 -0400

    added the two strips and darkmode also positioned the logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import Logo from "./assets/Logo.png"
 const App = () => {
   return (
     <div className="bg-white dark:bg-black w-screen h-screen">
-      <Name h1="font-robot text-black" div="bg-red-200"/>
-      <img className="w-100 h-100" src={Logo} />
+      <Name h1="font-robot text-black text-8xl text-black dark:text-white" div="inline-block z-10"/>
+      <img className="w-20 h-24 absolute right-0 top-0 z-50" src={Logo} placeholder="logo" />
+      <div className="w-30 h-full absolute flex space-x-4 top-0 left-24">
+        <div className="bg-yellow-500 w-10 h-full  "></div>
+        <div className="bg-blue-500 w-10 h-full "></div>
+      </div>
     </div>
   )
 };
