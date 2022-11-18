562c12fbb26163ca4c16e8e98c07e8b16b003d24
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-11-18 14:39:48 -0500

    moved and changed color to <Title />

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Name from "./Components/Name";
+import Title from "./Components/Title";
 import Logo from "./assets/Logo.png";
 const App = () => {
   return (
@@ -7,8 +8,12 @@ const App = () => {
         h1="font-robot text-black text-8xl text-black dark:text-white"
         div="inline-block absolute z-40 left-64 top-24 "
       />
+      <Title
+        div="bg-black absolute right-64 bottom-24"
+        h2="text-black  dark:text-white"
+      />
       <img
-        className="w-20 h-24 absolute right-0 top-0 z-50"
+        className="w-20 h-24 absolute right-4 top-4 z-50"
         src={Logo}
         placeholder="logo"
       />
