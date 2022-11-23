1e0c3a53c407d0ca77e685ba23f3ec348e83155a
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-11-22 22:34:25 -0500

    changed spelling

--- src/Components/Name.tsx
+++ src/Components/Name.tsx
@@ -1,17 +1,20 @@
-const Name = (props: {h1: string, div:string}) => {
-    let int = 0;
-    const FirstNameStr = "KENNEDY";
-    const SecondNameStr = "MAHORO";
-    const FirstNameArray = FirstNameStr.split("").map(a => (<span key={int++}>{a}</span>))
-    const SecondNameArray = SecondNameStr.split("").map(a => (<span key={int++}>{a}</span>))
+const Name = (props: { h1: string; div: string }) => {
+  let int = 0;
+  const FirstNameStr = "KENNEDY";
+  const SecondNameStr = "MAHORO";
+  const FirstNameArray = FirstNameStr.split("").map((a) => (
+    <span key={int++}>{a}</span>
+  ));
+  const SecondNameArray = SecondNameStr.split("").map((a) => (
+    <span key={int++}>{a}</span>
+  ));
 
-
-    return (
-        <div className={props.div}>
-            <h1 className={props.h1}>{FirstNameArray}</h1>
-            <h1 className={props.h1}>{SecondNameArray}</h1>
-        </div>
-    )
-}
+  return (
+    <div className={props.div}>
+      <h1 className={props.h1}>{FirstNameArray}</h1>
+      <h1 className={props.h1}>{SecondNameArray}</h1>
+    </div>
+  );
+};
 
 export default Name;
