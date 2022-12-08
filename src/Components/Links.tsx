eec2a388682ba94fbdea4aee3f9bb061d1f74e76
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-12-08 10:59:27 -0500

    added Blog to links, and added placeholders for the links

--- src/Components/Links.tsx
+++ src/Components/Links.tsx
@@ -1,14 +1,17 @@
 const Links = (props: { div: string; links: string }) => {
   return (
     <div className={props.div}>
-      <a className={` ${props.links} bg-yellow-400`} href="">
-        About
-      </a>
-      <a className={` ${props.links} bg-blue-500`} href="">
+      <a className={` ${props.links} bg-yellow-400`} href="#Projects">
         Projects
       </a>
-      <a className={` ${props.links} bg-yellow-400`} href="">
+      <a className={` ${props.links} bg-blue-500`} href="#Contact">
         Contact
+      </a>
+      <a className={` ${props.links} bg-yellow-400`} href="#About">
+        About
+      </a>
+      <a className={` ${props.links} bg-yellow-400`} href="#Blog">
+        Blog
       </a>
     </div>
   );
