7af9fb038ed97b24ac8b01034cbcc54b8e6852aa
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-01-06 14:07:29 -0500

    absolute -> fixed, and started working on #project

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,10 @@ const App = () => {
     <div className="bg-white dark:bg-black w-screen h-screen">
       <Name
         h1="font-Kaisei text-black text-4xl text-black dark:text-white"
-        div="absolute z-40 left-20 top-6 "
+        div="fixed z-40 left-20 top-6 "
       />
       <Title
-        div=" absolute left-32 top-16 "
+        div=" fixed left-32 top-16 "
         h2="font-Kaisei text-2xl text-black  dark:text-white"
       />
       <div className="w-72  absolute left-32 p-0 m-0 top-72">
@@ -44,7 +44,7 @@ const App = () => {
         </p>
       </div>
       <Links
-        div="absolute right-64 top-12"
+        div="fixed right-64 top-12"
         links="m-3 px-4 py-1 font-Kaisei text-1xl text-slate-700 dark:text-slate-100"
       />
       <img src={Color_of_Bg} className=" absolute right-0 bottom-4" />
@@ -57,6 +57,12 @@ const App = () => {
         div=" fixed left-10 bottom-10 flex flex-col "
         img="w-10 h-10 my-2"
       />
+      <div #project>
+        <div>
+          <h3>Projects: </h3>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
